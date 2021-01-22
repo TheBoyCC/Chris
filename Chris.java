8dffd61845998331961fee18cc960ef1d03ee41d
nitro63 <32526558+nitro63@users.noreply.github.com>
2021-01-22 14:55:05 +0000

    Add files via upload

---
+++ Chris.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chris;
+
+import java.util.Date;
+
+/**
+ *
+ * @author HP
+ */
+public class Chris {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] Week ={"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+        for(String day : Week){
+            System.out.println(day);
+        }
+    }
+    
+}
